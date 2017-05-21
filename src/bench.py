78c569d78d093c61e76945a4970d1bb9ed7a86f1
M. Caristan <m.caristan@hotmail.fr>
2017-05-21 16:12:18 +0200

    Script to benchmark matrix_forward

---
+++ src/bench.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+
+import time
+from sage.all import *
+from ore_algebra import *
+
+if __name__ == "__main__":
+    A,n = ZZ["n"].objgen()
+    R,Sn = OreAlgebra(A,"Sn").objgen()
+
+    fibo = Sn**2 - Sn - 1
+    ###   print (fibo.to_list([0,1],10))
+    ###   P,Q = fibo.forward_matrix_bsplit (10, 0)
+    ###   print (P*Matrix([[0],[0]])/Q)
+
+    maxOrder = 5
+    minOrder = 1
+    fiboTime = []
+    execTime = [[] for _ in range(minOrder,maxOrder+1)]
+    cond = [range(i) for i in range(minOrder,maxOrder+1)]
+    for i in [10, 100, 1000]:
+        lst = 0
+        fwdMtx = 0
+        nbExec = 10
+        for k in range(nbExec):
+            startLst = time.time()
+            fibo.to_list([0,1],i)
+            stopLst = time.time()
+            
+            startMtx = time.time()
+            P,Q = fibo.forward_matrix_bsplit (i,0)
+            P*Matrix([[0],[1]])/Q
+            stopMtx = time.time()
+
+            lst += (stopLst - startLst)*1000./nbExec
+            fwdMtx += (stopMtx - startMtx)*1000./nbExec
+
+        fiboTime.append((lst, fwdMtx))
+        print (i, "Done :", lst, "\t", fwdMtx)
+
+        for j in range(minOrder,maxOrder+1):
+            k = 0
+            while k<nbExec :
+                f = R.random_element (j)
+                # Using to_list method
+                startLst = time.time()
+                f.to_list (cond[j-minOrder], i)
+                stopLst = time.time()
+
+                # Using forward_matrix
+                startMtx = time.time()
+                P,Q = f.forward_matrix_bsplit (i,0)
+                if Q==0:
+                   continue
+                P*Matrix([[e] for e in cond[j-minOrder]])/Q
+                stopMtx = time.time()
+
+                lst += (stopLst - startLst)*1000./nbExec
+                fwdMtx += (stopMtx - startMtx)*1000./nbExec
+                k += 1
+            execTime[j-minOrder].append((lst, fwdMtx))
+            print (i, "Done :", lst, "\t", fwdMtx)
+
+
